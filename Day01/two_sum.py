04a7341378f52ca7e3251d573e4426fc684a5acd
ChiangMark <king03168@gmail.com>
2024-10-08 16:16:43 +0800

    create new file

---
+++ Day01/two_sum.py
@@ -0,0 +1,11 @@
+target = 5
+nums = [3,2,4]
+
+def solution(array):
+    k = 0
+    for i in array:
+        k += 1
+        if target - i in array[k:]:
+            return(k - 1, array[k:].index(target - i) + k)
+
+print(solution(nums))
