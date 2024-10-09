ff872229f5e2603944e069d987ed8605ea431119
ChiangMark <king03168@gmail.com>
2024-10-09 10:10:01 +0800

    feat: hash_method

--- Day01/two_sum.py
+++ Day01/two_sum.py
@@ -1,11 +1,14 @@
 target = 5
 nums = [3,2,4]
 
-def solution(array):
+def for_loop_solution(array):
     k = 0
     for i in array:
         k += 1
         if target - i in array[k:]:
             return(k - 1, array[k:].index(target - i) + k)
+        
+def hash_method(array):
+    pass
 
-print(solution(nums))
+print(for_loop_solution(nums))
